0281c82ef42a0facfb3d76ccd53f31b81ddb688d
nathanmalone99 <nathanmalone99@gmail.com>
2024-06-19 12:55:12 +0100

    signup basic functionality works

--- GameHubApp/src/app/pages/signup/signup.page.ts
+++ GameHubApp/src/app/pages/signup/signup.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
 })
-export class SignupPage implements OnInit {
+export class SignupPage {
 
-  constructor() { }
+  email: string = '';
+  password: string = '';
 
-  ngOnInit() {
+  constructor(private authService: AuthService, private navCtrl: NavController) { }
+
+  async signup() {
+    try {
+      const user = await this.authService.register(this.email, this.password);
+      console.log('Signup successful:', user);
+      this.navCtrl.navigateForward('/home');  // Navigate to home or any other page after successful signup
+    } catch (error) {
+      console.error('Signup error:', error);
+    }
   }
-
 }
