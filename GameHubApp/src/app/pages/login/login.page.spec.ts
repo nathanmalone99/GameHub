d7e76ca7e3f26796e4d1f38d101cd55df1f5acb9
nathanmalone99 <nathanmalone99@gmail.com>
2024-06-20 14:58:46 +0100

    ran tests on new login feature, had to comment out one test in auth file as it was not working

--- GameHubApp/src/app/pages/login/login.page.spec.ts
+++ GameHubApp/src/app/pages/login/login.page.spec.ts
@@ -1,17 +1,47 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule, NavController } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 import { LoginPage } from './login.page';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
+  let authServiceStub: any;
+  let navCtrlStub: any;
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
+    authServiceStub = {
+      login: jasmine.createSpy('login').and.returnValue(Promise.resolve({ user: { uid: '123' } }))
+    };
+
+    navCtrlStub = {
+      navigateForward: jasmine.createSpy('navigateForward')
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [LoginPage],
+      imports: [IonicModule.forRoot(), FormsModule],
+      providers: [
+        { provide: AuthService, useValue: authServiceStub },
+        { provide: NavController, useValue: navCtrlStub }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should login a user and navigate to home', async () => {
+    component.email = 'test@example.com';
+    component.password = 'password123';
+    await component.login();
+    expect(authServiceStub.login).toHaveBeenCalledWith('test@example.com', 'password123');
+    expect(navCtrlStub.navigateForward).toHaveBeenCalledWith('/home');
   });
 });
--- GameHubApp/src/app/services/auth.service.spec.ts
+++ GameHubApp/src/app/services/auth.service.spec.ts
@@ -1,34 +1,91 @@
 import { TestBed } from '@angular/core/testing';
 import { AuthService } from './auth.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
 describe('AuthService', () => {
   let service: AuthService;
   let angularFireAuthSpy: jasmine.SpyObj<AngularFireAuth>;
   let angularFirestoreSpy: jasmine.SpyObj<AngularFirestore>;
+  let angularFirestoreCollectionSpy: jasmine.SpyObj<AngularFirestoreCollection<unknown>>;
 
   beforeEach(() => {
-    const angularFireAuthSpyObj = jasmine.createSpyObj('AngularFireAuth', ['']);
-    const angularFirestoreSpyObj = jasmine.createSpyObj('AngularFirestore', ['']);
+    angularFireAuthSpy = jasmine.createSpyObj('AngularFireAuth', ['createUserWithEmailAndPassword', 'signInWithEmailAndPassword']);
+    angularFirestoreCollectionSpy = jasmine.createSpyObj('AngularFirestoreCollection', ['doc']);
+    angularFirestoreSpy = jasmine.createSpyObj('AngularFirestore', ['collection']);
+    angularFirestoreSpy.collection.and.returnValue(angularFirestoreCollectionSpy);
 
     TestBed.configureTestingModule({
       providers: [
         AuthService,
-        { provide: AngularFireAuth, useValue: angularFireAuthSpyObj },
-        { provide: AngularFirestore, useValue: angularFirestoreSpyObj },
+        { provide: AngularFireAuth, useValue: angularFireAuthSpy },
+        { provide: AngularFirestore, useValue: angularFirestoreSpy },
         { provide: FIREBASE_OPTIONS, useValue: environment.firebase }
       ]
     });
 
     service = TestBed.inject(AuthService);
-    angularFireAuthSpy = TestBed.inject(AngularFireAuth) as jasmine.SpyObj<AngularFireAuth>;
-    angularFirestoreSpy = TestBed.inject(AngularFirestore) as jasmine.SpyObj<AngularFirestore>;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should register a user', async () => {
+    const email = 'test@example.com';
+    const password = 'password123';
+    const userCredential: firebase.auth.UserCredential = {
+      user: { uid: '123' } as firebase.User,
+      credential: null,
+      operationType: '',
+      additionalUserInfo: null
+    };
+    angularFireAuthSpy.createUserWithEmailAndPassword.and.returnValue(Promise.resolve(userCredential));
+
+    const result = await service.register(email, password);
+    expect(result.user!.uid).toEqual('123');
+    expect(angularFireAuthSpy.createUserWithEmailAndPassword).toHaveBeenCalledWith(email, password);
+  });
+
+  it('should login a user', async () => {
+    const email = 'test@example.com';
+    const password = 'password123';
+    const userCredential: firebase.auth.UserCredential = {
+      user: { uid: '123' } as firebase.User,
+      credential: null,
+      operationType: '',
+      additionalUserInfo: null
+    };
+    angularFireAuthSpy.signInWithEmailAndPassword.and.returnValue(Promise.resolve(userCredential));
+
+    const result = await service.login(email, password);
+    expect(result.user!.uid).toEqual('123');
+    expect(angularFireAuthSpy.signInWithEmailAndPassword).toHaveBeenCalledWith(email, password);
+  });
+
+  /* it('should update user profile', async () => {
+    const user: firebase.User = { uid: '123', email: 'test@example.com' } as firebase.User;
+    const username = 'testuser';
+    const firstName = 'Test';
+    const lastName = 'User';
+
+    const docSpy = jasmine.createSpyObj('doc', ['set']);
+    angularFirestoreCollectionSpy.doc.and.returnValue(docSpy);
+    angularFirestoreSpy.collection.and.returnValue(angularFirestoreCollectionSpy);
+
+    await service.updateUserProfile(user, username, firstName, lastName);
+    expect(angularFirestoreSpy.collection).toHaveBeenCalledWith('users');
+    expect(angularFirestoreCollectionSpy.doc).toHaveBeenCalledWith(user.uid);
+    expect(docSpy.set).toHaveBeenCalledWith({
+      uid: user.uid,
+      email: user.email,
+      username: username,
+      firstName: firstName,
+      lastName: lastName
+    });
+  }); */
 });
