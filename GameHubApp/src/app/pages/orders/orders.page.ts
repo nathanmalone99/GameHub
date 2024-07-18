2141939f3062f5c50ea82f9ae594c0c45f191809
nathanmalone99 <nathanmalone99@gmail.com>
2024-07-18 15:29:55 +0100

    updated order page design

--- GameHubApp/src/app/pages/orders/orders.page.ts
+++ GameHubApp/src/app/pages/orders/orders.page.ts
@@ -9,12 +9,18 @@ import { OrderService } from 'src/app/services/order.service';
 export class OrdersPage implements OnInit {
 
   orders: any[] = [];
+  expandedOrders: boolean[] = [];
 
   constructor(private orderService: OrderService) {}
 
   ngOnInit() {
     this.orderService.getOrders().subscribe(orders => {
       this.orders = orders;
+      this.expandedOrders = new Array(orders.length).fill(false);
     });
+  }
+
+  toggleOrderDetails(index: number) {
+    this.expandedOrders[index] = !this.expandedOrders[index];
   }
 }
