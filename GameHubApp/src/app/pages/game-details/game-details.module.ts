f793866293f72d48cea822df9624ca007378ac91
nathanmalone99 <nathanmalone99@gmail.com>
2024-07-22 00:59:57 +0100

    upated star system to be yellow as well as the order of the reviews are displayed. Also added the navbar to game details as I thought I done that already

--- GameHubApp/src/app/pages/game-details/game-details.module.ts
+++ GameHubApp/src/app/pages/game-details/game-details.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { GameDetailsPageRoutingModule } from './game-details-routing.module';
 
 import { GameDetailsPage } from './game-details.page';
+import { SharedModule } from 'src/app/modules/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    SharedModule,
     GameDetailsPageRoutingModule
   ],
   declarations: [GameDetailsPage]
