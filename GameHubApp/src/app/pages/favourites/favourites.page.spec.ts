47edaff7335b500d5e60fc0fefee1560182c054d
nathanmalone99 <nathanmalone99@gmail.com>
2024-08-21 12:27:56 +0100

    added more tests to favourites page and sevice file

--- GameHubApp/src/app/pages/favourites/favourites.page.spec.ts
+++ GameHubApp/src/app/pages/favourites/favourites.page.spec.ts
@@ -3,10 +3,12 @@ import { of } from 'rxjs';
 import { FavouritesPage } from './favourites.page';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FavouritesService } from 'src/app/services/favourites.service';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
 import { AngularFireModule } from '@angular/fire/compat';
 import { firebaseMockConfig } from 'src/app/testing/firebase.mock';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 const mockFavorites = [
   { id: 1, name: 'Game 1', background_image: 'image1.jpg', released: '2020-01-01' },
@@ -30,24 +32,29 @@ describe('FavouritesPage', () => {
   let component: FavouritesPage;
   let fixture: ComponentFixture<FavouritesPage>;
   let favouritesService: jasmine.SpyObj<FavouritesService>;
-
-  
+  let router: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
-    const favouritesServiceSpy = jasmine.createSpyObj('FavouritesService', ['getFavorites', 'removeFromFavorites']);
+    const favouritesServiceSpy = jasmine.createSpyObj('FavouritesService', ['getFavorites', 'removeFromFavorites', 'updateGameStatus']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
     favouritesServiceSpy.getFavorites.and.returnValue(of(mockFavorites));
     favouritesServiceSpy.removeFromFavorites.and.returnValue(of(void 0));
+    favouritesServiceSpy.updateGameStatus.and.returnValue(of(void 0));
 
     await TestBed.configureTestingModule({
       declarations: [FavouritesPage],
       imports: [
         IonicModule.forRoot(),
         AngularFireModule.initializeApp(firebaseMockConfig),
-        AngularFirestoreModule
+        AngularFirestoreModule,
+        RouterTestingModule.withRoutes([]),
       ],
       providers: [
         { provide: AngularFirestore, useValue: angularFirestoreStub },
-        { provide: FavouritesService, useValue: favouritesServiceSpy }
+        { provide: FavouritesService, useValue: favouritesServiceSpy },
+        { provide: Router, useValue: routerSpy },
+        { provide: NavController, useValue: jasmine.createSpyObj('NavController', ['navigateForward', 'navigateBack']) },
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
@@ -55,20 +62,55 @@ describe('FavouritesPage', () => {
     fixture = TestBed.createComponent(FavouritesPage);
     component = fixture.componentInstance;
     favouritesService = TestBed.inject(FavouritesService) as jasmine.SpyObj<FavouritesService>;
-    fixture.detectChanges();
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 
   it('should load favorite games on init', () => {
+    fixture.detectChanges();
     expect(component.favoriteGames).toEqual(mockFavorites);
     expect(favouritesService.getFavorites).toHaveBeenCalled();
   });
 
   it('should remove game from favorites', () => {
+    fixture.detectChanges();
     component.removeFromFavorites('1');
     expect(favouritesService.removeFromFavorites).toHaveBeenCalledWith('1');
+  });
+
+  it('should update game status', () => {
+    fixture.detectChanges();
+    const gameId = '1';
+    const status = 'In Progress';
+  
+    component.updateGameStatus(gameId, status);
+
+    expect(favouritesService.updateGameStatus).toHaveBeenCalledWith(gameId, status);
+  });
+
+  it('should navigate to game details', () => {
+    fixture.detectChanges();
+    const gameId = '1';
+
+    component.goToGameDetails(gameId);
+
+    expect(router.navigate).toHaveBeenCalledWith(['/game-details', gameId]);
+  });
+
+  it('should navigate to achievements', () => {
+    fixture.detectChanges();
+    const gameId = '1';
+
+    component.goToAchievements(gameId);
+
+    expect(router.navigate).toHaveBeenCalledWith(['/achievements', gameId]);
+  });
+
+  it('should initialize with correct default values', () => {
+    expect(component.favoriteGames).toEqual([]);
   });
 });
--- GameHubApp/src/app/services/favourites.service.spec.ts
+++ GameHubApp/src/app/services/favourites.service.spec.ts
@@ -3,7 +3,7 @@ import { FavouritesService } from './favourites.service';
 import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { firebaseMockConfig } from '../testing/firebase.mock';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { AngularFireModule } from '@angular/fire/compat';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
@@ -20,8 +20,8 @@ describe('FavouritesService', () => {
     const collectionStub = {
       valueChanges: jasmine.createSpy('valueChanges').and.returnValue(of([{}])),
       doc: jasmine.createSpy('doc').and.returnValue({
-        set: jasmine.createSpy('set').and.returnValue(of(void 0)),
-        delete: jasmine.createSpy('delete').and.returnValue(of(void 0)),
+        set: jasmine.createSpy('set').and.returnValue(Promise.resolve(void 0)),
+        delete: jasmine.createSpy('delete').and.returnValue(Promise.resolve(void 0)),
         update: jasmine.createSpy('update').and.returnValue(Promise.resolve(void 0)),
       })
     };
@@ -83,6 +83,87 @@ describe('FavouritesService', () => {
       expect(afsMock.collection).toHaveBeenCalledWith('users/testUserId/favorites');
       expect(afsMock.collection().doc).toHaveBeenCalledWith('1');
       expect(afsMock.collection().doc().update).toHaveBeenCalledWith({ status });
+      done();
+    });
+  });
+
+  it('should not add to favorites if user is not authenticated', (done) => {
+    afAuthMock.authState = of(null);
+    const game = { id: 1, name: 'Test Game' };
+    service.addToFavorites(game).subscribe((result) => {
+      expect(afsMock.collection).not.toHaveBeenCalled();
+      expect(result).toBeNull();
+      done();
+    });
+  });
+
+  it('should return empty array when getting favorites if user is not authenticated', (done) => {
+    afAuthMock.authState = of(null);
+    service.getFavorites().subscribe((favorites) => {
+      expect(afsMock.collection).not.toHaveBeenCalled();
+      expect(favorites).toEqual([]);
+      done();
+    });
+  });
+
+  it('should not remove from favorites if user is not authenticated', (done) => {
+    afAuthMock.authState = of(null);
+    service.removeFromFavorites('1').subscribe((result) => {
+      expect(afsMock.collection).not.toHaveBeenCalled();
+      expect(result).toBeNull();
+      done();
+    });
+  });
+
+  it('should not update game status if user is not authenticated', (done) => {
+    afAuthMock.authState = of(null);
+    const gameId = 1;
+    const status = 'In Progress';
+    service.updateGameStatus(gameId, status).subscribe((result) => {
+      expect(afsMock.collection).not.toHaveBeenCalled();
+      expect(result).toBeNull();
+      done();
+    });
+  });
+
+  it('should handle errors when adding to favorites', (done) => {
+    afsMock.collection().doc().set.and.returnValue(Promise.reject('Error adding to favorites'));
+    const game = { id: 1, name: 'Test Game' };
+    service.addToFavorites(game).subscribe({
+      next: () => fail('Expected an error, but got a success response'),
+      error: (error) => {
+        expect(error).toBe('Error adding to favorites');
+        done();
+      },
+    });
+  });
+
+  it('should handle errors when removing from favorites', (done) => {
+    afsMock.collection().doc().delete.and.returnValue(Promise.reject('Error removing from favorites'));
+    service.removeFromFavorites('1').subscribe({
+      next: () => fail('Expected an error, but got a success response'),
+      error: (error) => {
+        expect(error).toBe('Error removing from favorites');
+        done();
+      },
+    });
+  });
+
+  it('should work with a different user ID', (done) => {
+    afAuthMock.authState = of({ uid: 'anotherUserId' });
+    const game = { id: 1, name: 'Test Game' };
+    service.addToFavorites(game).subscribe(() => {
+      expect(afsMock.collection).toHaveBeenCalledWith('users/anotherUserId/favorites');
+      expect(afsMock.collection().doc).toHaveBeenCalledWith('1');
+      expect(afsMock.collection().doc().set).toHaveBeenCalledWith(game);
+      done();
+    });
+  });
+
+  it('should handle empty authState', (done) => {
+    afAuthMock.authState = of(null);
+    service.getFavorites().subscribe((favorites) => {
+      expect(favorites).toEqual([]);
       done();
     });
   });
