8e60b47d6b812c3dad57988d1eaa3a36bad4c213
nathanmalone99 <nathanmalone99@gmail.com>
2024-08-31 18:13:38 +0100

    updated credentials

--- GameHubApp/src/environments/environment.prod.ts
+++ GameHubApp/src/environments/environment.prod.ts
@@ -1,16 +1,14 @@
 export const environment = {
   production: true,
   stripePublicKey: 'pk_test_51N7gd7IB2R5TmgiivsbQfGz3Xix7juEdTtoQyzViy3smc2wQGR3RilkZoQgsFbsaaX2EZk5HKC2OZbntbslHQTKn00g0xqAmSM',
-  firebaseFunctionUrl: 'https://europe-west1-gamehub-e4466.cloudfunctions.net/createPaymentIntent',
   
   firebase: {
-    apiKey: "AIzaSyAzSwu7Jjb5yWlKjP21bLwj3uKti4qf_9k",
-    authDomain: "gamehub-e4466.firebaseapp.com",
-    projectId: "gamehub-e4466",
-    storageBucket: "gamehub-e4466.appspot.com",
-    messagingSenderId: "837968484208",
-    appId: "1:837968484208:web:28b943a8f7bc829e66a1b5",
-    measurementId: "G-C9T3D73HW7"
+    apiKey: "AIzaSyCy6vBupB9cJXtPgklEYSqHtpYb0Ig6pmY",
+    authDomain: "gamehub3-25198.firebaseapp.com",
+    projectId: "gamehub3-25198",
+    storageBucket: "gamehub3-25198.appspot.com",
+    messagingSenderId: "229510203757",
+    appId: "1:229510203757:web:ea477870207aba154fb264"
   },
   rawgApiKey: '6f16f1bd149e41ae9eaf8f3a0a394e5b'
 };
--- GameHubApp/src/environments/environment.ts
+++ GameHubApp/src/environments/environment.ts
@@ -1,15 +1,14 @@
 export const environment = {
   production: false,
   stripePublicKey: 'pk_test_51N7gd7IB2R5TmgiivsbQfGz3Xix7juEdTtoQyzViy3smc2wQGR3RilkZoQgsFbsaaX2EZk5HKC2OZbntbslHQTKn00g0xqAmSM',
-  firebaseFunctionUrl: 'https://europe-west1-gamehub-e4466.cloudfunctions.net/createPaymentIntent',
   
   firebase: {
-    apiKey: "AIzaSyD6TzPnV53InvLGillohpmqiczgsAu-tyU",
-  authDomain: "gamehub2-f7680.firebaseapp.com",
-  projectId: "gamehub2-f7680",
-  storageBucket: "gamehub2-f7680.appspot.com",
-  messagingSenderId: "996580759380",
-  appId: "1:996580759380:web:912ac59c1e1beb58c81ac1"
+    apiKey: "AIzaSyCy6vBupB9cJXtPgklEYSqHtpYb0Ig6pmY",
+    authDomain: "gamehub3-25198.firebaseapp.com",
+    projectId: "gamehub3-25198",
+    storageBucket: "gamehub3-25198.appspot.com",
+    messagingSenderId: "229510203757",
+    appId: "1:229510203757:web:ea477870207aba154fb264"
   },
   rawgApiKey: '6f16f1bd149e41ae9eaf8f3a0a394e5b'
 };
